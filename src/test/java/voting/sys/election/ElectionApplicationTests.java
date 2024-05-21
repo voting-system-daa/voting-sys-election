25624646d3ec2d639f566b5e66ff2d8f884f225d
Denis Trifan <trifan.denis1999@gmail.com>
2024-05-21 10:03:36 +0300

    added initial project files

---
+++ src/test/java/voting/sys/election/ElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package voting.sys.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
